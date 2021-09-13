a4ec5b6d428eee8609fb4686ebed877f1681e6d0
amila-92 <amila@101digital.io>
2021-09-13 12:51:47 +0100

    Binary tree

--- src/main/java/test/tree/BinaryTreeTest1.java
+++ src/main/java/test/tree/BinaryTreeTest1.java
@@ -1,6 +1,6 @@
 package test.tree;
 
-public class BinaryTree {
+class BinaryTreeTest1 {
 
     public static void main(String args[]){
 
---
+++ src/main/java/test/tree/BinaryTreeTest2.java
@@ -0,0 +1,13 @@
+package test.tree;
+
+import org.apache.poi.util.BinaryTree;
+
+public class BinaryTreeTest2 {
+
+    public static void main(String args[]){
+        BinaryTree binaryTree = new BinaryTree();
+
+        //binaryTree.put();
+    }
+
+}
